5e9dd6d33146295ee1e34dd5598ffc3bc80d3d43
YoavF <yoavfeldman4@gmail.com>
2026-01-24 02:26:04 +0200

    transition hash to shared_ptd and move from recur to loops

--- HashMap.h
+++ HashMap.h
@@ -1,134 +1,164 @@
 //
 // Created by yoavf on 08/01/2026.
 //
-#include  <memory>
+#include <memory>
 #include <iostream>
 #include <exception>
 
 using namespace std;
+
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
 template <typename T>
-class HashMap{
+class HashMap {
 private:
-    struct Node
-    {
+    struct Node {
         shared_ptr<Node> next;
         int key;
-        T data;
-        Node(): key(-1), data(new T) {}
-        Node(T val, int key): data(val), key(key), next(nullptr){}
+        shared_ptr<T> data;
+
+        Node() : key(-1), data(nullptr), next(nullptr) {}
+        Node(const T& val, int key)
+            : key(key), data(make_shared<T>(val)), next(nullptr) {}
         ~Node() = default;
     };
 
     int length;
     int count;
-    Node<T>* objects;
+
+    // array of buckets
+    shared_ptr<shared_ptr<Node>[]> objects;
 
     int hash(int key) {
-        return  key % size;
+        return key % length;
     }
 
-    void dive(Node<T> curr, HashMap<T>* hashmap) {
-        hashmap->put(curr.key, curr.data);
-        if (curr->next != nullptr) {
-            dive(curr->next, hashmap);
+    void dive(shared_ptr<Node> curr, HashMap<T>* hashmap) {
+        while (curr != nullptr) {
+            hashmap->put(*(curr->data), curr->key);
+            curr = curr->next;
         }
     }
 
     void resize() {
-        HashMap hold = HashMap(length*2);
-        for(int i = 0; i < length; i++) {
-            dive(objects[i], hold);
+        HashMap hold = HashMap(length * 2);
+        for (int i = 0; i < length; i++) {
+            dive(objects[i], &hold);
         }
-        length*=2;
-        delete objects;
-        objects = hold->objects;
-        count = hold->count;
-
+        length *= 2;
+        objects = hold.objects;
+        count = hold.count;
     }
-
-
 
 public:
-    HashMap(): length(8), count(0) {
-        objects = new Node<T>*[length]();
+    HashMap() : length(8), count(0) {
+        objects = shared_ptr<shared_ptr<Node>[]>(
+            new shared_ptr<Node>[length](),
+            default_delete<shared_ptr<Node>[]>()
+        );
     }
 
-    explicit HashMap(int len): length(len), count(0) {
-        objects = new Node<T>*[length]();
+    explicit HashMap(int len) : length(len), count(0) {
+        objects = shared_ptr<shared_ptr<Node>[]>(
+            new shared_ptr<Node>[length](),
+            default_delete<shared_ptr<Node>[]>()
+        );
     }
 
-    virtual ~HashMap() {
-        delete[] objects;
+    ~HashMap() = default;
+
+    bool isEmpty() const {
+        return count == 0;
     }
 
-    bool isEmpty() const{return count == 0;}
-
-    bool traverse(Node<T> curr, int key) {
-        if(curr.key == key){return true;}
-        if(curr->next == nullptr){return false;}
-        return traverse(curr->next, key);
+    bool traverse(shared_ptr<Node> curr, int key) {
+        while (true) {
+            if (curr->key == key) return true;
+            if (curr->next == nullptr) return false;
+            curr = curr->next;
+        }
     }
 
-    bool contains (int key) const {
+    bool contains(int key) const {
         int location = hash(key);
-        if(objects[location] == nullptr){return false;}
+        if (objects[location] == nullptr) return false;
         return traverse(objects[location], key);
     }
 
-    void insert(Node<T> curr, T data, int key) {
-        if (curr.next == nullptr){curr.next = new Node<T>(data, key); return;}
-        insert(curr->next, data, key);
+    void insert(shared_ptr<Node> curr, const T& data, int key) {
+        while (curr->next != nullptr) {
+            curr = curr->next;
+        }
+        curr->next = make_shared<Node>(data, key);
     }
 
-    void put(const T& data, int key)
-    {
-        if(contains(key)){throw invalid_argument("already exists");}
+    void put(const T& data, int key) {
+        if (contains(key)) {
+            throw invalid_argument("already exists");
+        }
+
         int location = hash(key);
-        insert(objects[location], data, key);
+
+        if (objects[location] == nullptr) {
+            objects[location] = make_shared<Node>(data, key);
+        } else {
+            insert(objects[location], data, key);
+        }
+
         count += 1;
-        if (((double) count) / length >= 0.75) {
+
+        if ((double)count / length >= 0.75) {
             resize();
         }
     }
 
-    void extract(Node<T> curr, int key) {
-        if (curr->next.key == key) {
+    void extract(shared_ptr<Node> curr, int key) {
+        while (true) {
+            if (curr->next->key == key) {
                 curr->next = curr->next->next;
+                return;
+            }
+            curr = curr->next;
         }
-        extract(curr->next, key);
     }
 
-    void remove(int key)
-    {
-        if(!contains(key)){throw invalid_argument("not present");}
-        int location = hash(key);
-        if (objects[location].key == key) {
-            next = objects[location].next;
+    void remove(int key) {
+        if (!contains(key)) {
+            throw invalid_argument("not present");
         }
-        extract(objects[location], key);
+
+        int location = hash(key);
+
+        if (objects[location]->key == key) {
+            objects[location] = objects[location]->next;
+        } else {
+            extract(objects[location], key);
+        }
+
         count -= 1;
     }
 
-    T locate(Node<T> curr, int key) {
-        if (curr.key == key){return curr.data;}
-        locate(curr->next, key);
+    T locate(shared_ptr<Node> curr, int key) {
+        while (true) {
+            if (curr->key == key) {
+                return *(curr->data);
+            }
+            curr = curr->next;
+        }
     }
 
     T get(int key) {
-        if(!contains(key)){throw invalid_argument("not present");}
+        if (!contains(key)) {
+            throw invalid_argument("not present");
+        }
         int location = hash(key);
-        return extract(objects[location], key);
+        return locate(objects[location], key);
     }
 
-    int size(){return count;}
-
-
+    int size() {
+        return count;
+    }
 };
 
-
-
-
-#endif //HASHMAP_H
+#endif // HASHMAP_H
