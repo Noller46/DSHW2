fd4adb95b2b6ea03494b018eaa7dbb468131bba0
RonCarmi <ron.carmi15@gmail.com>
2026-01-25 19:46:22 +0200

    fewminormethods2

--- Huntech26a2.cpp
+++ Huntech26a2.cpp
@@ -12,13 +12,36 @@ StatusType Huntech::add_squad(int squadId) {
         return StatusType::INVALID_INPUT;
     try
     {
-        
+        Squad newSquad(squadId);
+        squads.insert(newSquad,squadId);
+        return StatusType::SUCCESS;
     }
-    return StatusType::FAILURE;
+    catch (const std::bad_alloc&)
+    {
+        return StatusType::ALLOCATION_ERROR;
+    }
+    catch (const std::invalid_argument&)
+    {
+        return StatusType::FAILURE;
+    }
+    catch (...){
+        return StatusType::FAILURE;
+    }
 }
 
 StatusType Huntech::remove_squad(int squadId) {
-    return StatusType::FAILURE;
+    if (squadId <= 0)
+        return StatusType::INVALID_INPUT;
+    try
+    {
+        auto node = squads.find(squadId);
+        squads.remove(squadId);
+        return StatusType::SUCCESS;
+    }
+    catch (...)
+    {
+        return StatusType::FAILURE;
+    }
 }
 
 StatusType Huntech::add_hunter(int hunterId,
@@ -35,6 +58,20 @@ output_t<int> Huntech::squad_duel(int squadId1, int squadId2) {
 }
 
 output_t<int> Huntech::get_hunter_fights_number(int hunterId) {
+    if (hunterId <= 0)
+        return StatusType::INVALID_INPUT;
+    try
+    {
+        if (!hunters.contains(hunterId))
+            return StatusType::FAILURE;
+        std::shared_ptr<MemberNode> node = hunters.get(hunterId);
+        FindResult res = node->find();
+        return output_t<int>(res.pathFights);
+    }
+    catch (...)
+    {
+        return {StatusType::FAILURE};
+    }
     return 0;
 }
 
