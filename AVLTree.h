c7e753ad8aa773d1ab008563a5f3782d2971b2de
YoavF <yoavfeldman4@gmail.com>
2026-01-25 14:33:30 +0200

    fixed hight and below issues

--- AVLTree.h
+++ AVLTree.h
@@ -15,63 +15,94 @@ class AVLTree : public BinaryTree<T> {
     if(!node) return 0;
     return this->getHeight(node->left_son)-this->getHeight(node->right_son);
   }
-    shared_ptr<Node> rotate_left(shared_ptr<Node>& a) // abs(bf)>=2
-    {
-      if (!a || !a->right_son)
-        return a;
-      shared_ptr<Node> b = a->right_son;
-      shared_ptr<Node> parent = a->parent.lock();
-      int below_a = a->below();
-      a->right_son = b->left_son;
-      if (a->right_son) {
-        a->right_son->parent = a;
-        a->below = a->below - b->below + a->right_son->below;
-      }
-      b->left_son = a;
-      b->below = below_a;
-      b->parent = a->parent;
-      a->parent = b;
-      if (parent)
-      {
-        if (parent->left_son == a)
-          parent->left_son = b;
-        else
-          parent->right_son = b;
-      }else{
-        this->root = b;
-      }
-      this->updateHeight(a);
-      this->updateHeight(b);
-      return b;
+  shared_ptr<Node> rotate_left(shared_ptr<Node>& a)
+  {
+    if (!a || !a->right_son)
+      return a;
 
+    shared_ptr<Node> b = a->right_son;
+    shared_ptr<Node> parent = a->parent.lock();
+
+    a->right_son = b->left_son;
+    if (a->right_son)
+      a->right_son->parent = a;
+
+    b->left_son = a;
+    a->parent = b;
+
+    b->parent = parent;
+    if (parent)
+    {
+      if (parent->left_son == a)
+        parent->left_son = b;
+      else
+        parent->right_son = b;
     }
+    else
+    {
+      this->root = b;
+    }
+
+    a->below = 0;
+    if (a->left_son)
+      a->below += a->left_son->below + 1;
+    if (a->right_son)
+      a->below += a->right_son->below + 1;
+
+    b->below = 0;
+    if (b->left_son)
+      b->below += b->left_son->below + 1;
+    if (b->right_son)
+      b->below += b->right_son->below + 1;
+
+    this->updateHeight(a);
+    this->updateHeight(b);
+
+    return b;
+  }
   shared_ptr<Node> rotate_right(shared_ptr<Node>& c)
   {
     if (!c || !c->left_son)
       return c;
+
     shared_ptr<Node> b = c->left_son;
     shared_ptr<Node> parent = c->parent.lock();
-    int below_c = c->below();
+
     c->left_son = b->right_son;
-    b->right_son = c;
-    b->parent = c->parent;
-    c->parent = b;
-    if (c->left_son) {
+    if (c->left_son)
       c->left_son->parent = c;
-      c->below = c->below - b->below + c->left_son->below;
-    }
-    b->below = below_c;
+
+    b->right_son = c;
+    c->parent = b;
+
+    b->parent = parent;
     if (parent)
     {
       if (parent->left_son == c)
         parent->left_son = b;
       else
         parent->right_son = b;
-    }else{
+    }
+    else
+    {
       this->root = b;
     }
+
+    c->below = 0;
+    if (c->left_son)
+      c->below += c->left_son->below + 1;
+    if (c->right_son)
+      c->below += c->right_son->below + 1;
+
+    b->below = 0;
+    if (b->left_son)
+      b->below += b->left_son->below + 1;
+    if (b->right_son)
+      b->below += b->right_son->below + 1;
+
     this->updateHeight(c);
     this->updateHeight(b);
+
     return b;
   }
   void balance(shared_ptr<Node>& node)
@@ -100,23 +131,25 @@ class AVLTree : public BinaryTree<T> {
       node = rotate_left(node);
     }
   }
-  void insert_recursive(shared_ptr<Node>& curr, const T& val, int key, weak_ptr<Node> parent) override
+  bool insert_recursive(shared_ptr<Node>& curr, const T& val, int key, weak_ptr<Node> parent) override
   {
-    BinaryTree<T>::insert_recursive(curr, val, key, parent);
+    bool ret = BinaryTree<T>::insert_recursive(curr, val, key, parent);
     if (curr)
     {
       this->updateHeight(curr);
       balance(curr);
     }
+    return ret;
   }
-  void remove_recursive(shared_ptr<Node>& curr, int key)override
+  bool remove_recursive(shared_ptr<Node>& curr, int key)override
   {
-    BinaryTree<T>::remove_recursive(curr, key);
+    bool ret = BinaryTree<T>::remove_recursive(curr, key);
     if (curr)
     {
       this->updateHeight(curr);
       balance(curr);
     }
+    return ret;
   }
 public:
   AVLTree() = default;
--- BinaryTree.h
+++ BinaryTree.h
@@ -3,7 +3,7 @@
 //
 #include  <memory>
 #include <iostream>
-#include <exception>
+#include <stdexcept>
 
 using namespace std;
 #ifndef BINARYTREE_H
@@ -21,12 +21,12 @@ protected:
             T data;
             int height;
             int below;
-            explicit Node(T val): data(val), height(1), below(0){}
+            explicit Node(T val, int key): data(val), key(key), height(1), below(0){}
             ~Node() = default;
         };
     shared_ptr<Node> root;
 
-    // unwrap helpers
+    /*
     template<typename U>
     static const U& unwrap(const U& v) { return v; }
 
@@ -35,6 +35,7 @@ protected:
 
     template<typename U>
     static const U& unwrap(const std::unique_ptr<U>& p) { return *p; }
+*/
 
     int getHeight(const shared_ptr<Node>& node)const{
         return node? node->height:0;
@@ -48,33 +49,46 @@ protected:
             node->height = 1 + (left_height > right_height ? left_height : right_height);
         }
     }
-    virtual void insert_recursive(shared_ptr<Node>& curr, const T& val, int key, weak_ptr<Node> parent){
-        if (!curr)
-        {
-            curr = make_shared<Node>(val);
+    virtual bool insert_recursive(shared_ptr<Node>& curr, const T& val, int key, weak_ptr<Node> parent){
+        if (!curr) {
+            curr = make_shared<Node>(val, key);
             curr->parent = parent;
-            return;
+            updateHeight(curr);
+            return true;
         }
-        int currKey = curr->key;
 
-        if (key < currKey)
-            insert_recursive(curr->left_son, val, key, curr);
-        else if (currKey < key)
-            insert_recursive(curr->right_son, val, key, curr);
-        else
+        if (key < curr->key) {
+            if (insert_recursive(curr->left_son, val, key, curr))
+                curr->below++;
+        }
+        else if (curr->key < key) {
+            if (insert_recursive(curr->right_son, val, key, curr))
+                curr->below++;
+        }
+        else {
             throw invalid_argument("Already here!");
-
-        curr->below += 1;
+        }
+        updateHeight(curr);
+        return true;
     }
-    virtual void remove_recursive(shared_ptr<Node>& curr, int key)
+
+    virtual bool remove_recursive(shared_ptr<Node>& curr, int key)
     {
         if (!curr)
             throw invalid_argument("Object not in tree!");
         int currKey = curr->key;
         if (key < currKey)
-            remove_recursive(curr->left_son, key);
+            if (remove_recursive(curr->left_son, key)) {
+                curr->below -= 1;
+                updateHeight(curr);
+                return true;
+            }
         else if (currKey < key)
-            remove_recursive(curr->right_son, key);
+            if (remove_recursive(curr->right_son, key)) {
+                curr->below -= 1;
+                updateHeight(curr);
+                return true;
+            }
         else
         {
             if (!curr->right_son)
@@ -82,12 +96,16 @@ protected:
                 auto child = curr->left_son;
                 if (child) child->parent = curr->parent;
                 curr = child;
+                updateHeight(curr);
+                return true;
             }
             else if (!curr->left_son)
             {
                 auto child = curr->right_son;
                 if (child) child->parent = curr->parent;
                 curr = child;
+                updateHeight(curr);
+                return true;
             }
             else
             {
@@ -96,10 +114,15 @@ protected:
                     successor = successor->left_son;
 
                 curr->data = successor->data;
-                remove_recursive(curr->right_son,successor->key);
+                curr->key = successor->key;
+                if (remove_recursive(curr->right_son,successor->key)) {
+                    curr->below--;
+                    updateHeight(curr);
+                    return true;
+                }
             }
         }
-        curr->below -= 1;
+        throw invalid_argument("Unexpected error");
     }
     shared_ptr<Node> find_recursive(const shared_ptr<Node>& curr, int key)const
     {
@@ -134,7 +157,7 @@ protected:
     void print_in_order_recursive(const shared_ptr<Node>& node) const {
         if (!node) return;
         print_in_order_recursive(node->left_son);
-        cout << *node->data << "(H:" << node->height << ") ";
+        cout << node->data << "(H:" << node->height << ") ";
         print_in_order_recursive(node->right_son);
     }
     void print() const {
